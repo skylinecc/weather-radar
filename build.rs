066e973ab3cbdea0037359f74aaddb07182d9d7b
DefunctLizard <grantshandy@gmail.com>
2020-09-24 18:55:41 -0600

    simplify

--- build.rs
+++
@@ -1,9 +0,0 @@
-use std::process::Command;
-
-fn main() {
-    let _ = Command::new("sh")
-                        .args(&["-c", "cd data && glib-compile-resources resources.xml"])
-                        .output()
-                        .expect("failed to build gresource");
-
-}
