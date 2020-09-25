43419f4abecda122326c8cdb02b7d22700794ec8
DefunctLizard <grantshandy@gmail.com>
2020-09-25 14:18:59 -0600

    almost done working on national

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,6 @@ fn main() {
 		gio::ApplicationFlags::FLAGS_NONE)
 		.expect("Application::new failed");
 
-	let gif = ureq::get("https://radar.weather.gov/ridge/Conus/Loop/northrockies_loop.gif").call().into_string().unwrap();
-
-	let mut file = std::fs::File::create("map.gif").expect("create failed");
-	file.write_all(gif.as_bytes()).expect("write failed");
-
 //	Load the compiled resource bundle
 	let resources_bytes = include_bytes!("../data/resources.gresource");
 	let resource_data = glib::Bytes::from(&resources_bytes[..]);
@@ -31,10 +26,14 @@ fn main() {
         let builder = Builder::new_from_resource("/org/skylinecc/WeatherRadar/menu.ui");
 		let popover_menu: gtk::PopoverMenu = builder.get_object("popover_menu").expect("Couldn't get popov);_menu");
 		let about_button: gtk::ModelButton = builder.get_object("about_button").expect("Couldn't get about_button");
+		let close_button: gtk::ModelButton = builder.get_object("close_button").expect("Couldn't get close_button");
 
         let location_chooser = gtk::ComboBoxText::new();
+        location_chooser.append(Some("national"), "National");
         location_chooser.append(Some("northrockies"), "Northern Rocky Mountains");
         location_chooser.append(Some("southrockies"), "Southern Rocky Mountains");
+
+        location_chooser.set_active_id(Some("national"));
 
         let menu_btn = gtk::MenuButton::new();
         menu_btn.set_image(Some(&gtk::Image::new_from_icon_name(Some("open-menu-symbolic"), gtk::IconSize::SmallToolbar)));
@@ -47,6 +46,9 @@ fn main() {
 		header_bar.add(&location_chooser);
 		header_bar.pack_end(&menu_btn);
 
+        national();
+        let national_gif = gtk::Image::new_from_file("/tmp/national.gif");
+
         northrockies();
         let northrockies_gif = gtk::Image::new_from_file("/tmp/northrockies.gif");
 
@@ -56,14 +58,17 @@ fn main() {
         let map_stack = gtk::Stack::new();
         map_stack.add_named(&northrockies_gif, "north_rockies");
         map_stack.add_named(&southrockies_gif, "south_rockies");
+        map_stack.add_named(&national_gif, "national");
 
-        map_stack.set_visible_child(&northrockies_gif);
+        map_stack.set_visible_child(&national_gif);
 
         location_chooser.connect_changed(clone!(@weak map_stack => move |location_chooser| {
             if location_chooser.get_active_id().unwrap() == "northrockies" {
                 map_stack.set_visible_child(&northrockies_gif);
             } else if location_chooser.get_active_id().unwrap() == "southrockies" {
                 map_stack.set_visible_child(&southrockies_gif);
+            } else if location_chooser.get_active_id().unwrap() == "national" {
+                map_stack.set_visible_child(&national_gif);
             }
         }));
 
@@ -71,14 +76,19 @@ fn main() {
         window_frame.pack_start(&map_stack, true, true, 0);
 
         // Then we set its size and a title.
-        win.set_default_size(500, 500);
+        win.set_default_size(600, 571);
         win.set_titlebar(Some(&header_bar));
+//        win.set_resizable(false);
 
         win.add(&window_frame);
 
         about_button.connect_clicked(move |_| {
         	about();
         });
+
+        close_button.connect_clicked(clone!(@weak win => move |_| {
+        	win.destroy();
+        }));
 
         // Don't forget to make all widgets visible.
         win.show_all();
@@ -87,35 +97,39 @@ fn main() {
     uiapp.run(&env::args().collect::<Vec<_>>());
 }
 
-fn northrockies() {
+fn national() {
+    std::fs::remove_file("/tmp/national.gif");
+	let mut file = std::fs::File::create("/tmp/national.gif").expect("create failed");
 
-    std::fs::remove_file("/tmp/northrockies.gif");
-
-	let mut file = std::fs::File::create("/tmp/northrockies.gif").expect("create failed");
-
-    let mut gif_r = ureq::get("https://radar.weather.gov/ridge/Conus/Loop/northrockies_loop.gif").call().into_reader();
-
+    let mut gif_r = ureq::get("https://radar.weather.gov/ridge/Conus/Loop/NatLoop_Small.gif").call().into_reader();
     let mut gif_w: Vec<u8> = vec![];
 
     std::io::copy(&mut gif_r, &mut gif_w);
+	file.write_all(&gif_w).expect("write failed");
+}
 
+fn northrockies() {
+    std::fs::remove_file("/tmp/northrockies.gif");
+	let mut file = std::fs::File::create("/tmp/northrockies.gif").expect("create failed");
+
+    let mut gif_r = ureq::get("https://radar.weather.gov/ridge/Conus/Loop/northrockies_loop.gif").call().into_reader();
+    let mut gif_w: Vec<u8> = vec![];
+
+    std::io::copy(&mut gif_r, &mut gif_w);
 	file.write_all(&gif_w).expect("write failed");
 }
 
 fn southrockies() {
-
     std::fs::remove_file("/tmp/southrockies.gif");
-
 	let mut file = std::fs::File::create("/tmp/southrockies.gif").expect("create failed");
 
     let mut gif_r = ureq::get("https://radar.weather.gov/ridge/Conus/Loop/southrockies_loop.gif").call().into_reader();
-
     let mut gif_w: Vec<u8> = vec![];
 
     std::io::copy(&mut gif_r, &mut gif_w);
-
 	file.write_all(&gif_w).expect("write failed");
 }
+
 fn about() {
 	let about_window = AboutDialog::new();
 	about_window.set_website_label(Some("Weather Radar"));
