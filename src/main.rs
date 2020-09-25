6d13929f16e53f43aacef3e548b58f6cb3c2a98b
DefunctLizard <grantshandy@gmail.com>
2020-09-24 20:06:52 -0600

    finish basic UI

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,60 @@
 use gio::prelude::*;
 use gtk::prelude::*;
+use gtk::{License::Gpl30, Builder, AboutDialog};
 use std::env;
-use gtk::AboutDialog;
-use gtk::License::Gpl30;
 
 fn main() {
-    let uiapp = gtk::Application::new(Some("org.skylinecc.WeatherRadar"),
-                                      gio::ApplicationFlags::FLAGS_NONE)
-                                 .expect("Application::new failed");
-    uiapp.connect_activate(|app| {
-        // We create the main window.
+	let uiapp = gtk::Application::new(Some("org.skylinecc.WeatherRadar"),
+		gio::ApplicationFlags::FLAGS_NONE)
+		.expect("Application::new failed");
+
+//	Load the compiled resource bundle
+	let resources_bytes = include_bytes!("../data/resources.gresource");
+	let resource_data = glib::Bytes::from(&resources_bytes[..]);
+	let res = gio::Resource::new_from_data(&resource_data).unwrap();
+	gio::resources_register(&res);
+
+//	Load the window UI
+	uiapp.connect_activate( |app| {
         let win = gtk::ApplicationWindow::new(app);
-        let header_bar = gtk::HeaderBar::new();
 
-        header_bar.set_title(Some("Radar Viewer"));
-        header_bar.set_show_close_button(true);
+        let builder = Builder::new_from_resource("/org/skylinecc/WeatherRadar/menu.ui");
+		let popover_menu: gtk::PopoverMenu = builder.get_object("popover_menu").expect("Couldn't get popov);_menu");
+		let about_button: gtk::ModelButton = builder.get_object("about_button").expect("Couldn't get about_button");
 
-        let location_chooser = gtk::ComboBoxText::new();
-        header_bar.add(&location_chooser);
-
-        let popover_menu = gtk::PopoverMenu::new();
-        let popover_box = gtk::Box::new(gtk::Orientation::Vertical, 0);
-        popover_menu.set_property_width_request(100);
-        popover_menu.add(&popover_box);
-
-        let about_button = gtk::ModelButton::new();
-        let menu_separator = gtk::Separator::new(gtk::Orientation::Vertical);
-        let quit_button = gtk::ModelButton::new();
-
-        popover_box.pack_start(&about_button, false, false, 0);
-        popover_box.pack_start(&menu_separator, false, false, 0);
-        popover_box.pack_start(&quit_button, false, false, 0);
+//        let location_chooser = gtk::ComboBoxText::new();
 
         let menu_btn = gtk::MenuButton::new();
         menu_btn.set_image(Some(&gtk::Image::new_from_icon_name(Some("open-menu-symbolic"), gtk::IconSize::SmallToolbar)));
         menu_btn.set_use_popover(true);
         menu_btn.set_popover(Some(&popover_menu));
 
-        header_bar.pack_end(&menu_btn);
+        let header_bar = gtk::HeaderBar::new();
+        header_bar.set_title(Some("Radar Viewer"));
+        header_bar.set_show_close_button(true);
+//		header_bar.add(&location_chooser);
+		header_bar.pack_end(&menu_btn);
 
         let window_frame = gtk::Box::new(gtk::Orientation::Vertical, 0);
 
         // Then we set its size and a title.
-        win.set_default_size(320, 200);
+        win.set_default_size(500, 500);
         win.set_titlebar(Some(&header_bar));
 
         win.add(&window_frame);
 
+        about_button.connect_clicked(move |_| {
+        	about();
+        });
+
         // Don't forget to make all widgets visible.
         win.show_all();
     });
+
     uiapp.run(&env::args().collect::<Vec<_>>());
 }
 
 fn about() {
-
 	let about_window = AboutDialog::new();
 	about_window.set_website_label(Some("Weather Radar"));
 	about_window.set_website(Some("https://github.com/skylinecc/weather-radar"));
@@ -79,5 +79,4 @@ fn about() {
 		"Alex Rose",
 	]);
     about_window.show_all();
-
 }
